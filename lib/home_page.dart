660d522f510698a98d7d4afb4b2de965f7b4bc52
Olivier AFOUTOU <livermakolz@gmail.com>
2025-04-23 23:32:45 +0000

    Second tp, décoration container et utilisation du wifget Expanded

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,25 +19,54 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 200,
-            // width: 500,
-            color: Colors.amber,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(height: 150, width: 100, color: Colors.black),
-                Container(height: 150, width: 100, color: Colors.white),
-                Container(height: 150, width: 100, color: Colors.red),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 200,
+              // width: 500,
+              color: Colors.amber,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  topContainer(Colors.black, 'assets/images/1.jpg', 150, 100),
+                  topContainer(Colors.red, 'assets/images/2.jpg', 150, 100),
+                  topContainer(Colors.amber, 'assets/images/3.jpg', 150, 100),
+                  // Expanded(
+                  //   child: Container(
+                  //     height: 150,
+                  //     // width: 300,
+                  //     decoration: BoxDecoration(
+                  //       color: Colors.blueAccent,
+                  //       borderRadius: BorderRadius.circular(20),
+                  //       image: DecorationImage(
+                  //         image: AssetImage('assets/images/3.jpg'),
+                  //         fit: BoxFit.cover,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(height: 700, color: Colors.green),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget topContainer(Color color, String image, double height, double width) {
+  return Container(
+    height: height,
+    width: width,
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(20),
+      image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+    ),
+  );
 }
