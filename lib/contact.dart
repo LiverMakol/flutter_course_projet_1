552f507392ddc86bc4cda72b3debb1f07b52e67e
Olivier AFOUTOU <livermakolz@gmail.com>
2025-04-30 23:30:29 +0000

    Caroussel slider

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,38 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:projet_1/tab_page.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class Contact extends StatelessWidget {
-  const Contact({Key? key}) : super(key: key);
+class Contact extends StatefulWidget {
+  Contact({Key? key}) : super(key: key);
+
+  @override
+  State<Contact> createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  List imgList = [
+    {'id': 1, 'image_path': 'assets/images/1.jpg'},
+    {'id': 2, 'image_path': 'assets/images/2.jpg'},
+    {'id': 3, 'image_path': 'assets/images/3.jpg'},
+  ];
+
+  int valeur = 0;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
         title: Text('Contacts'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Text('$valeur', style: TextStyle(fontSize: 25)),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            valeur++;
+          });
+          print(valeur);
+        },
+        child: Icon(Icons.add),
       ),
       body: Column(
         children: [
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.green,
-              image: DecorationImage(
-                image: AssetImage('assets/images/1.jpg'),
-                fit: BoxFit.cover,
+          Stack(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200,
+                    viewportFraction: 1,
+                    autoPlay: true,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                  ),
+                  items:
+                      imgList
+                          .map(
+                            (item) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 5.0,
+                                horizontal: 12.0,
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                    image: AssetImage(item['image_path']),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                ),
               ),
-            ),
+              Positioned(
+                bottom: 10,
+                left: 0,
+                right: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:
+                      imgList.map((element) {
+                        int index = imgList.indexOf(element);
+                        return Container(
+                          height: 20,
+                          width: 20,
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 2,
+                            vertical: 2,
+                          ),
+                          decoration: BoxDecoration(
+                            color:
+                                currentIndex == index
+                                    ? Colors.white
+                                    : Colors.grey,
+                            shape: BoxShape.circle,
+                          ),
+                        );
+                      }).toList(),
+                ),
+              ),
+            ],
           ),
           Expanded(
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.grey[300],
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
+                // borderRadius: BorderRadius.only(
+                //   topLeft: Radius.circular(20),
+                //   topRight: Radius.circular(20),
+                // ),
               ),
               child: ListView.separated(
                 separatorBuilder: (context, index) {
